a45cd34818fc0a4ec8113346c93f81edc5d8d339
Berlin Johnson <berlinjohnson@Berlins-Mac-mini.local>
2024-01-06 23:07:21 -0600

    worked on UI

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import TodoForm from "./TodoForm";
 import axios from "axios";
+import { TextField, Container, ListItem, List, Button, Checkbox } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Home() {
   const [todos, setTodos] = useState([]);
@@ -19,41 +23,58 @@ function Home() {
       .put(`http://localhost:5000/update/${id}`)
       .then((result) => {
         // setTodos(result.data);
-        console.log(result)
+        console.log(result);
       })
       .catch((err) => console.log(err));
   };
+
   const handleDelete = (id) => {
     axios
       .delete(`http://localhost:5000/delete/${id}`)
       .then((result) => {
-        window.location.reload()
+        window.location.reload();
       })
       .catch((err) => console.log(err));
   };
   return (
-    <div className="container">
-      <h2>Todo List</h2>
+    <Container className="container">
       <TodoForm />
-      <ul>
+      <List className="list">
         {todos.map((todo) => (
-          <li key={todo._id}>
-            <button
-              type="button"
-              className="edit"
+          <ListItem key={todo._id} className="list-item">
+            <Checkbox 
+              onChange={() => handleEdit(todo._id)}
+            />
+            {/* <Button
+              variant="contained"
+              color="success"
               // pass id into function to send to server to update
               onClick={() => handleEdit(todo._id)}
             >
               Done
-            </button>
+            </Button> */}
             {todo.task}
-            <button type="button" className="del" onClick={() => handleDelete(todo._id)}>
+            {/* <Button
+              variant="contained"
+              color="error"
+              className="del"
+              onClick={() => handleDelete(todo._id)}
+            >
               Delete
-            </button>
-          </li>
+            </Button> */}
+            <EditIcon 
+              color="primary"
+            />
+            <DeleteIcon 
+            fontSize="small"
+            className="del"
+            color="error"
+            onClick={() => handleDelete(todo._id)}
+            />
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Container>
   );
 }
 
--- client/src/components/TodoForm.js
+++ client/src/components/TodoForm.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { TextField,Button } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
+
 
 function TodoForm() {
   const [task, setTask] = useState();
@@ -10,10 +15,23 @@ function TodoForm() {
       .catch((err) => console.log(err));
   };
   return (
-    <div>
-      <input type="text" onChange={e => setTask(e.target.value)} placeholder="Enter a task"/>
-      <button type="button" onClick={handleTask}>Add</button>
-    </div>
+    <>
+    <TextField
+      variant="outlined"
+      label="Enter a task"      
+      value={task}
+      onChange={e => setTask(e.target.value)} 
+      autoFocus
+    />
+
+
+      {/* <Button variant="contained" onClick={handleTask}>Add</Button> */}
+      <AddBoxRoundedIcon
+        fontSize="large"
+        color="success"
+        className="add"
+      />
+    </>
   );
 }
 
